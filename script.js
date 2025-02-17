b2f9188ab43e51de26e8c30314705bba497dbd28
Ren Aoki <49857703+rensanrenren@users.noreply.github.com>
2025-02-17 12:33:54 +0900

    credit

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/rensan/cm695riwn00fr01stf1ef0tap',
   projection: 'globe', // 地球儀表示
+  customAttribution: 'NASA Lunar Reconnaissance Orbiter',
   zoom: 2,
   minzoom: 2,
   maxzoom: 8,
